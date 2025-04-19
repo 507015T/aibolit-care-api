e503c9d902c7b22c44b9dc8cbb21d70d4d717bc0
507015T <cuztrippie666@gmail.com>
2025-04-19 18:54:34 +0200

    refactor(schedules): improve annotations, relation

--- src/schedules/models.py
+++ src/schedules/models.py
@@ -1,18 +1,25 @@
 from typing import Optional
-from sqlalchemy import Column, Integer, String, SmallInteger, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
-from database import Base
+from sqlalchemy import String, SmallInteger, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from database import Base, intpk
 from datetime import date
 
 
 class MedicationSchedule(Base):
     __tablename__ = "schedules"
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[intpk]
     medication_name: Mapped[str] = mapped_column(
         String(255), index=True, nullable=False
     )
     frequency: Mapped[int] = mapped_column(SmallInteger, index=True, nullable=False)
     duration_days: Mapped[Optional[int]] = mapped_column(SmallInteger)
     start_date: Mapped[date] = mapped_column(default=date.today(), nullable=False)
-    end_date: Mapped[Optional[date]] = mapped_column(nullable=True, default=None)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    end_date: Mapped[Optional[date]]
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.policy_number", ondelete="CASCADE"), nullable=False
+    )
+
+    user: Mapped["User"] = relationship("User", back_populates="schedules")
+
+
+
