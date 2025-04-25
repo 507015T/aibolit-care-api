a1bb5a7533cb38df6d4cda9dae7e16f1763d9422
507015T <cuztrippie666@gmail.com>
2025-04-25 19:12:56 +0200

    feat: add calc end_date, filter current intakes

--- src/schedules/service.py
+++ src/schedules/service.py
@@ -1,21 +1,31 @@
-from schedules import models
+from datetime import date, timedelta
+from operator import or_
+
 from sqlalchemy import select
-from datetime import date
+
+from schedules import models
 
 
 async def create_schedule(schedule, db):
-    db_schedule = models.MedicationSchedule(**schedule.model_dump())
+    end_date = schedule.start_date + timedelta(days=schedule.duration_days) if schedule.duration_days else None
+    db_schedule = models.MedicationSchedule(end_date=end_date, **schedule.model_dump())
     db.add(db_schedule)
     await db.commit()
     await db.refresh(db_schedule)
-    return db_schedule
+    return {"schedule_id": db_schedule.id}
 
 
 async def get_schedules(user_id, db):
 
     result = await db.execute(
         select(models.MedicationSchedule)
-        .where(models.MedicationSchedule.user_id == user_id)
+        .filter(models.MedicationSchedule.user_id == user_id)
+        .filter(
+            or_(
+                models.MedicationSchedule.end_date >= date.today(),
+                models.MedicationSchedule.end_date is None,
+            ),
+        ),
     )
 
     schedules = result.scalars().all()
