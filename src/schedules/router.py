b105d6fb00f2792423dde01000e14c8ea6c90069
507015T <cuztrippie666@gmail.com>
2025-04-27 21:31:10 +0200

    refactor: Update import to 'schedules.schemas'

--- src/schedules/router.py
+++ src/schedules/router.py
@@ -3,53 +3,62 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing_extensions import Annotated
 
 import database
-from schedules import schemas, service, utils
+from schedules import utils, service
+
+from schedules.schemas import (
+    MedicationScheduleCreateResponse,
+    MedicationScheduleCreate,
+    MedicationScheduleIdsResponse,
+    MedicationSchedule,
+    NextTakingsMedications,
+    NextTakingsMedicationsResponse,
+)
 from users.service import validate_user_exists
 
 router = APIRouter()
 
 
-@router.post("/schedule", status_code=201, response_model=schemas.MedicationScheduleCreateResponse)
+@router.post("/schedule", status_code=201, response_model=MedicationScheduleCreateResponse)
 async def create_schedule(
-    schedule: schemas.MedicationScheduleCreate,
+    schedule: MedicationScheduleCreate,
     db: Annotated[AsyncSession, Depends(database.get_db)],
-) -> schemas.MedicationScheduleCreateResponse:
+) -> MedicationScheduleCreateResponse:
     user_exists = await validate_user_exists(schedule.user_id, db)
     if not user_exists:
         raise HTTPException(status_code=404, detail=f"User with id={schedule.user_id} not found")
     db_schedule = await service.create_schedule(schedule, db)
-    return schemas.MedicationScheduleCreateResponse(schedule_id=db_schedule.id)
+    return MedicationScheduleCreateResponse(schedule_id=db_schedule.id)
 
 
-@router.get("/schedules", response_model=schemas.MedicationScheduleIdsResponse)
+@router.get("/schedules", response_model=MedicationScheduleIdsResponse)
 async def get_all_schedules(
     user_id: int, db: Annotated[AsyncSession, Depends(database.get_db)]
-) -> schemas.MedicationScheduleIdsResponse:
+) -> MedicationScheduleIdsResponse:
     db_schedules = await service.get_schedules(user_id, db)
-    schedules = [schemas.MedicationSchedule.model_validate(schedule).id for schedule in db_schedules]
-    return schemas.MedicationScheduleIdsResponse(user_id=user_id, schedules=schedules)
+    schedules = [MedicationSchedule.model_validate(schedule).id for schedule in db_schedules]
+    return MedicationScheduleIdsResponse(user_id=user_id, schedules=schedules)
 
 
-@router.get("/schedule", response_model=schemas.MedicationSchedule)
+@router.get("/schedule", response_model=MedicationSchedule)
 async def get_user_schedule(
     schedule_id: int, user_id: int, db: Annotated[AsyncSession, Depends(database.get_db)]
-) -> schemas.MedicationSchedule:
+) -> MedicationSchedule:
     db_schedule = await service.get_user_schedule(schedule_id, user_id, db)
-    schedule = schemas.MedicationSchedule.model_validate(db_schedule)
+    schedule = MedicationSchedule.model_validate(db_schedule)
     return schedule
 
 
-@router.get("/next_takings", response_model=schemas.NextTakingsMedicationsResponse)
+@router.get("/next_takings", response_model=NextTakingsMedicationsResponse)
 async def get_user_next_takings(
     user_id: int, db: Annotated[AsyncSession, Depends(database.get_db)]
-) -> schemas.NextTakingsMedicationsResponse:
+) -> NextTakingsMedicationsResponse:
     db_schedules = await service.get_schedules(user_id, db)
-    schedules = [schemas.MedicationSchedule.model_validate(schedule) for schedule in db_schedules]
+    schedules = [MedicationSchedule.model_validate(schedule) for schedule in db_schedules]
     next_takings = [
-        schemas.NextTakingsMedications(
+        NextTakingsMedications(
             schedule_id=next_taking.id, schedule_name=next_taking.medication_name, schedule_times=next_taking.daily_plan
         )
         for next_taking in schedules
         if any(map(utils.is_within_timeframe, next_taking.daily_plan))
     ]
-    return schemas.NextTakingsMedicationsResponse(user_id=user_id, next_takings=next_takings)
+    return NextTakingsMedicationsResponse(user_id=user_id, next_takings=next_takings)
