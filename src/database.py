63d0d85d698f4570f210526415367813467dc165
507015T <cuztrippie666@gmail.com>
2025-05-02 18:01:20 +0200

    feat: add context manager for get_db for grpc

--- src/database.py
+++ src/database.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from typing import Annotated
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
@@ -16,12 +17,19 @@ SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False, autoflush
 class Base(DeclarativeBase): ...
 
 
-async def get_db():
+# for grpc
+@asynccontextmanager
+async def get_db_ctx():
     db = SessionLocal()
     try:
         yield db
     finally:
         await db.close()
+
+
+async def get_db():
+    async with get_db_ctx() as db:
+        yield db
 
 
 intpk = Annotated[int, mapped_column(primary_key=True, index=True)]
