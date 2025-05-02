4d5c64cbf84bd0ff3d479472db490e134e9fd9ef
507015T <cuztrippie666@gmail.com>
2025-05-02 19:57:34 +0200

    feat: add start grpc and rest in parallel

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env -S uv run --script --no-dev
+import asyncio
 import uvicorn
 from fastapi import FastAPI
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent / ".." / "grpc_server"))
+from grpc_main import serve as serve_grpc
 from schedules.router import router as schedules_router
 from users.router import router as users_router
 
@@ -9,5 +15,15 @@ app.include_router(schedules_router)
 app.include_router(users_router)
 
 
+async def serve_rest():
+    config = uvicorn.Config("main:app", host="localhost", port=8000, reload=True)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+
+async def main():
+    await asyncio.gather(serve_grpc(), serve_rest())
+
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    asyncio.run(main())
